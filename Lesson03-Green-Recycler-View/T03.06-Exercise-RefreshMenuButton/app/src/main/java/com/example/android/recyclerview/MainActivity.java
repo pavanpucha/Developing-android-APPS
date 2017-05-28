fe04ff020419723cab519ada9852b020b9251ad3
pavanpucha <pavanrajasekhar@gmail.com>
2017-05-27 23:41:14 -0400

    T3.06 Soolution

--- Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -79,7 +81,24 @@ public class MainActivity extends AppCompatActivity {
     // TODO (7) Override onCreateOptionsMenu
     // TODO (8) Use getMenuInflater().inflate to inflate the menu
     // TODO (9) Return true to display this menu
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+            return true;
+        }
+  @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+      int p = item.getItemId();
+      if(p == R.id.action_refresh){
+          mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+          mNumbersList.setAdapter(mAdapter);
+          return true;
 
+      }
+       else
+          return super.onOptionsItemSelected(item);
+
+    }
     // TODO (10) Override onOptionsItemSelected
     // TODO (11) Within this method, get the ID from the MenuItem
     // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
