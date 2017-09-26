38b698c346f5b9f0d8b8d016edc9cf717c03648a
pavanpucha <pavanrajasekhar@gmail.com>
2017-09-26 17:15:16 -0400

    databases

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,13 +1,42 @@
 package com.example.android.waitlist.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.android.waitlist.data.WaitlistContract.WaitlistEntry;
 // TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+public class WaitlistDbHelper extends SQLiteOpenHelper {
 
-    // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+    @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_WAITLIST_TABLE = "Create Table " + WaitlistContract.WaitlistEntry.TABLE_NAME + "("
+                + WaitlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + WaitlistEntry.COLUMN_GUEST_NAME + " TEXT NOT NULL,"
+                + WaitlistEntry.COLUMN_PARTY_SIZE + " INTEGER NOT NULL,"
+                +WaitlistEntry.COLUMN_TIMESTAMP+ " TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
+                + ");";
+        sqLiteDatabase.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
 
-    // TODO (3) Create a static final int called DATABASE_VERSION and set it to 1
 
-    // TODO (4) Create a Constructor that takes a context and calls the parent constructor
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + WaitlistEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+    private static final String DATABASE_NAME = "waitlist.db";
+
+    private static final int DATABASE_VERSION =1;
+
+    public WaitlistDbHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    // COMPLETED (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
+
+    // COMPLETED (3) Create a static final int called DATABASE_VERSION and set it to 1
+
+    // COMPLETED (4) Create a Constructor that takes a context and calls the parent constructor
 
     // TODO (5) Override the onCreate method
 
